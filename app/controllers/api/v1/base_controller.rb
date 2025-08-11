37a9afa590410a4073ce25b93f40c22c81e529c5
Renato Rosa Franco <renato_ny@live.com>
2025-08-11 11:41:02 -0300

    refactor: clean work around code for tests.

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -5,15 +5,7 @@ module API
     class BaseController < ActionController::API
       include Devise::Controllers::Helpers
 
-      before_action :purge_session
       before_action :authenticate_user!
-
-      private
-
-      def purge_session
-        request.session_options[:skip] = true
-        session.clear if respond_to?(:session) && session
-      end
     end
   end
 end
